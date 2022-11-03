15bcd078f3fd071be0f7ec33351f875ae9303a38
emrul-hoque <emrul.hoque@gmail.com>
2022-11-03 22:33:36 +0600

    update

--- Program.cs
+++ Program.cs
@@ -9,20 +9,54 @@ public class Program
     {
         DisplayUsers();
 
-        Console.WriteLine("Enter \n1 to add new Name \n2 to delete record \n3 to view records ");
-        int i = Convert.ToInt32(Console.ReadLine());
 
-        switch (i)
+        var optContinue = true;
+
+        while (optContinue)
         {
-            case 1:
-                AddUserRecord("Mozammel");
-                break;
-        }
-        //DeleteTable("user");
-        //CreateTable();
-        //UpdateUserRecord(2, "Rufana");
+            Console.WriteLine("Enter \n1 to add new Name \n2 to delete record \n3 to update a record");
+            int i = Convert.ToInt32(Console.ReadLine());
 
-        //DeleteUserRecord(3);
+            switch (i)
+            {
+                case 1:
+                    Console.WriteLine("Enter family member's name: ");
+                    string name = Console.ReadLine();
+                    AddUserRecord(name);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter the record number of the person that needs to be deleted ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    DeleteUserRecord(num);
+                    break;
+
+                case 3:
+                    Console.WriteLine("\nEnter the record number of the person that you want to edit and then type the person's name");
+                    int record = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"User enterted: {record}");
+                    Console.WriteLine("\nNow enter the name of the person");
+                    string? person = Console.ReadLine();
+                    Console.WriteLine($"User enterted: {person}");
+                    UpdateUserRecord(record, person);
+                    break;
+
+            }
+
+            Console.WriteLine("Press Y to continue or N to quit");
+            var readUserOpt = Console.ReadLine();
+           
+            if (readUserOpt?.ToUpper() == "N" )
+            {
+                optContinue = false;
+            }
+        }
+    }
+
+    private static void PromptUserForOpt()
+    {
+        Console.WriteLine("Press Y to continue or N to quit");
+        var readUserOpt = Console.ReadLine();
     }
 
     public static void CreateTable()
@@ -65,7 +99,9 @@ public class Program
 
             command.CommandText =
                             $"UPDATE user SET name = '{name}' WHERE id = {id}";
-            command.ExecuteNonQuery();
+            var i = command.ExecuteNonQuery();
+
+            Console.WriteLine($"Affected rows: {i}");
         }
     }
 
